14049d4383cc962b04676595e222cff9b58f55fd
Georgi Dimitrov <gogo3888@mail.bg>
2020-09-28 09:47:13 +0300

    Update zabbix-agent-install.sh

--- zabbix-agent-install.sh
+++ zabbix-agent-install.sh
@@ -6,6 +6,7 @@
 ###vars###
 command=rpm
 os=$(grep -E '^NAME' /etc/*release|awk -F '\"' '{print $2}')
+Host=$(hostname -f)
 
 $command -ivh zabbix-agent-4.4.6-*
 
@@ -26,10 +27,7 @@ cp  zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf
 mkdir -p /usr/lib/zabbix/externalscripts/ 
 cp oscheck.sh /usr/lib/zabbix/externalscripts/
 ####SETUP ZABBIX CONFIGURATON FILE##########################################################
-sed -i 's/Server=*/Server=10.1.52.27/g' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/ServerActive=*/ServerActive=10.1.52.27:10050/g' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/Hostname=*/Hostname=$(hostname -f)/' /etc/zabbix/zabbix_agentd.conf 
-sed -  's/TLSPSKIdentity=zabbix/TLSPSKIdentity=zabbix_test/g' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/^Hostname=/Hostname=$Host/' /etc/zabbix/zabbix_agentd.conf 
 #############################################################################################
 chown -R zabbix: /var/run/zabbix/
 chown -R zabbix: /usr/lib/zabbix/
